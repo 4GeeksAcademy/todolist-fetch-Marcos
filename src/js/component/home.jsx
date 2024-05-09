6e223c52969db7f788807674b5dfc17bc6edd6b4
Marcos Cabral Peralta <156668187+Marcos-93LAB@users.noreply.github.com>
2024-05-09 10:34:03 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,58 +1,103 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [newTodo, setNewTodo] = useState("")
-	const [todos, setTodos] = useState("")
+  const [inputValue, setInputValue] = useState("");
+  const [Todos, setTodos] = useState([]);
 
-	const handleClick = () => {
-		return(
-			setTodos([...todos, newTodo])
-		)
-	}
+ 
 
-	const deleteTask = (index) => {
-		const listaNueva = todos.filter((todo, i) => i !== index)
-		setTodos(listaNueva)
-	}
+  const raw = JSON.stringify(Todos);
 
-	const handleChange = (event) => {
-		return(
-			setNewTodo(event.target.value)
-		)
-	}
+//   const PostFetch = () => {
+//     const requestOptions = {
+//       method: "POST",
+//       redirect: "follow",
+//       body: raw,
+//     };
 
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Todo list!</h1>
-			<div className="container">
-				<input type="text" className="form-control" onChange={handleChange} />
-				<button 
-				type="button" 
-				className="btn btn-success"
-				onClick={handleClick}
-				>
-				Agregar tarea</button>
-			
-				<ul>
-					{todos.map((todo) => {
-						return(
-							<li>
-								{todo} <button 
-								type="button" 
-								className="btn btn-danger"
-								onClick={deleteTask}
-								>Borrar</button>
-							</li>
-						)
-					})}
-				</ul>
-			</div>
-		</div>
-	);
+//     fetch("https://playground.4geeks.com/todo/todos/marcos", requestOptions)
+//       .then((response) => response.json())
+//       .then((result) => console.log(result))
+//       .catch((error) => console.error(error));
+//   };
+
+  const getTodos = () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+
+    fetch("https://playground.4geeks.com/todo/users/marcos", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+		console.log(result)
+		setTodos(result.todos);
+	})
+      .catch((error) => console.error(error));
+  };
+
+  	useEffect(() =>{
+		getTodos();
+	}, [])
+
+  const handlekeydown = (e) => {
+    if (e.key === "Enter") {
+      if (inputValue.trim().length >= 3) {
+        Todos.push(inputValue);
+        console.log(Todos);
+        setInputValue("");
+        PostFetch();
+      } else {
+        alert("Por favor, escriba alguna tarea.");
+      }
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Mis Tareas</h1>
+      <ul className="paper">
+        <input
+          type="text"
+          placeholder="¿Qué necesitas hacer?"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+          onKeyDown={handlekeydown}
+        />
+
+        {Todos.map((item, index) => {
+          return (
+            <div className="contenedor">
+              <li key={index}>
+                {" "}
+                {item}
+                <i
+                  className="fa-solid fa-trash"
+                  onClick={() =>
+                    setTodos((prevTodos) =>
+                      prevTodos.filter((todo) => todo !== item)
+                    )
+                  }
+                ></i>
+              </li>
+            </div>
+          );
+        })}
+      </ul>
+      <div className="counter">{Todos.length} tareas</div>
+      <div className="one"></div>
+      <div className="two"></div>
+    </div>
+  );
 };
 
 export default Home;
